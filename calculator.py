486eb785cf01989a1483506570363b25b6ef41a2
Rashidjuma198 <rashidjumaoti@gmail.com>
2025-03-06 13:55:56 +0300

    The main code to be implemented through collaboration

---
+++ calculator.py
@@ -0,0 +1,8 @@
+"""
+This module contains functions for basic arithmetic operations: addition, multiplication, and subtraction.
+
+Functions:
+    add(a, b): Returns the sum of a and b. (To be implemented by Ian)
+    multiply(a, b): Returns the product of a and b. (To be implemented by Rashid)
+    subtract(a, b): Returns the difference between a and b. (To be implemented by Gladys)
+"""
